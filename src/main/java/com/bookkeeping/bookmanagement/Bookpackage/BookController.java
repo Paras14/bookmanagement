b690618256cea3cd6a6791e491d8faad7d4947c0
Paras14 <53565432+Paras14@users.noreply.github.com>
2023-10-15 23:29:42 +0530

    Add book retrieval Mappings

--- src/main/java/com/bookkeeping/bookmanagement/Bookpackage/BookController.java
+++ src/main/java/com/bookkeeping/bookmanagement/Bookpackage/BookController.java
@@ -1,6 +1,10 @@
 package com.bookkeeping.bookmanagement.Bookpackage;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BookController {
@@ -11,9 +15,14 @@ public class BookController {
         this.bookService = bookService;
     }
 
-//    @GetMapping("/books")
-//    public List<Book> getAllBooks(){
-//
-//    }
+    @GetMapping("/books")
+    public List<Book> getAllBooks(){
+        return bookService.retrieveAllBooks();
+    }
+
+    @GetMapping("books/{isbn}")
+    public Book retrieveBook(@PathVariable String isbn){
+        return bookService.findByIsbn(isbn);
+    }
 
 }
