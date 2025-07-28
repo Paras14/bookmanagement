59ad2a1ab9e0e1562c10867ccbff0256f070dc53
paras <parasv249@gmail.com>
2025-07-28 12:59:11 +0200

    Rename old CRA frontend to frontend-cra

--- backend/src/main/java/com/bookkeeping/bookmanagement/book/configuration/CorsConfiguration.java
+++ backend/src/main/java/com/bookkeeping/bookmanagement/book/configuration/CorsConfiguration.java
@@ -15,7 +15,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("Authorization")
